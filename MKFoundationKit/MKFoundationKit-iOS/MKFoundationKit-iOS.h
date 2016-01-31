47f1a78653c89c9806b68eb80a096657af1e6d6b
Stephen Panaro <stephen.panaro@gmail.com>
2016-01-31 16:54:57 -0500

    Add shared iOS framework scheme

---
+++ MKFoundationKit/MKFoundationKit-iOS/MKFoundationKit-iOS.h
@@ -0,0 +1,19 @@
+//
+//  MKFoundationKit-iOS.h
+//  MKFoundationKit-iOS
+//
+//  Created by Stephen Panaro on 1/31/16.
+//  Copyright © 2016 Michal Konturek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKFoundationKit-iOS.
+FOUNDATION_EXPORT double MKFoundationKit_iOSVersionNumber;
+
+//! Project version string for MKFoundationKit-iOS.
+FOUNDATION_EXPORT const unsigned char MKFoundationKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKFoundationKit_iOS/PublicHeader.h>
+
+
